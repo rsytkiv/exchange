e41e034441b00cd6e626ad5830923a830dff16cf
Roman Sytkiv <67324068+rsytkiv@users.noreply.github.com>
2023-03-28 23:13:44 +0300

    reverted webpack fix

--- src/components/Rules/index.js
+++ src/components/Rules/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { RuleTemplate } from './RuleTemplate.js';
+import { RuleTemplate } from './RuleTemplate';
 
 import { RULES_LABELS, RULES_OBJECT } from './content';
 
